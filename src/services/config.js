a929ff9f80652f89a9ad574ed0b1176b0ebf457d
Jack Park <jackpark@gmail.com>
2019-12-06 16:48:18 -0800

    added queries

--- src/services/config.js
+++ src/services/config.js
@@ -10,7 +10,21 @@ const config = {
   APP_GRP_PIV: "/hyp/grppiv",
   APP_TAG_PIV: "/hyp/tagpiv",
   APP_USR_PIV: "/hyp/usrpiv",
-  APP_RES_PIV: "/hyp/respiv"
+  APP_RES_PIV: "/hyp/respiv",
+
+  HYP_RES_BY_USR:"/hyp/resusr",
+  HYP_RES_BY_TAG: "/hyp/restg",
+  HYP_RES_BY_GRP: "/hyp/resgrp",
+  HYP_TAG_BY_USR: "/hyp/tagusr",
+  HYP_TAG_BY_RES: "/hyp/tagres",
+  HYP_TAG_BY_GRP: "/hyp/taggrp",
+  HYP_GRP_BY_USR: "/hyp/grpusr",
+  HYP_GRP_BY_TAG: "/hyp/grptag",
+  HYP_GRP_BY_RES: "/hyp/grpres",
+  HYP_USR_BY_RES: "/hyp/usrres",
+  HYP_USR_BY_TAG: "/hyp/usrtag",
+  HYP_USR_BY_GRP: "/hyp/usrgrp"
+
 };
 
 export default config;
